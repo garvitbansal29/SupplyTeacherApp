33e60197654dd5d08267d5359cfbbafea7a24049
johnny <johnny.molyneux@gmail.com>
2020-02-20 21:57:43 +0000

    this part is done, UI looks all over the shop but fuck it man

--- app/src/main/java/com/example/supplyteacherapp/SchoolRegisterDetail.java
+++ app/src/main/java/com/example/supplyteacherapp/SchoolRegisterDetail.java
@@ -2,10 +2,8 @@ package com.example.supplyteacherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,8 +12,8 @@ public class SchoolRegisterDetail extends AppCompatActivity {
     String username, schoolName, postcode,addressLine1, addressLine2, town, county, phone, email, subjects;
     boolean drivingLicense, DBS;
     int yearsOfExperience;
-    EditText schoolName_field, postcode_field, addressLine1_field, addressLine2_field, town_field, county_field, phone_field, yearsOfExperience_field, subjects_field;
-    CheckBox drivingLicense_checkBox, dbs_checkBox;
+    EditText name_Field, school_Field, postcode_field, addressLine1_field, addressLine2_field, town_field, county_field, phone_field;
+    //CheckBox drivingLicense_checkBox, dbs_checkBox;
 
     Database database = new Database(this);
     @Override
@@ -24,17 +22,18 @@ public class SchoolRegisterDetail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school_register_detail);
 
-        schoolName_field = findViewById(R.id.name_field);
+        name_Field = findViewById(R.id.name_field);
+        school_Field = findViewById(R.id.schoolInput);
         postcode_field = findViewById(R.id.postcode_field);
         addressLine1_field = findViewById(R.id.addressLine1_field);
         addressLine2_field = findViewById(R.id.addressLine2_field);
         town_field = findViewById(R.id.town_field);
         county_field = findViewById(R.id.county_field);
         phone_field = findViewById(R.id.phoneNumber_field);
-        drivingLicense_checkBox = findViewById(R.id.drivingLicense_checkBox);
-        dbs_checkBox = findViewById(R.id.DBS_checkBox);
-        yearsOfExperience_field = findViewById(R.id.yearsOfExperience_field);
-        subjects_field = findViewById(R.id.subjects_field);
+        //drivingLicense_checkBox = findViewById(R.id.drivingLicense_checkBox);
+        //dbs_checkBox = findViewById(R.id.DBS_checkBox);
+        //yearsOfExperience_field = findViewById(R.id.yearsOfExperience_field);
+        //subjects_field = findViewById(R.id.subjects_field);
 
 
     }
@@ -43,18 +42,18 @@ public class SchoolRegisterDetail extends AppCompatActivity {
     {
         username = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-        schoolName = schoolName_field.getText().toString();
+        schoolName = school_Field.getText().toString();
         postcode = postcode_field.getText().toString();
         addressLine1 = addressLine1_field.getText().toString();
         addressLine2 = addressLine2_field.getText().toString();
         town = town_field.getText().toString();
         county = county_field.getText().toString();
         phone = phone_field.getText().toString();
-        drivingLicense = drivingLicense_checkBox.isChecked();
-        DBS = dbs_checkBox.isChecked();
-        String stringYearsOfExperience = yearsOfExperience_field.getText().toString();
-        yearsOfExperience = new Integer(stringYearsOfExperience).intValue();
-        subjects = subjects_field.getText().toString();
+        //drivingLicense = drivingLicense_checkBox.isChecked();
+        //DBS = dbs_checkBox.isChecked();
+        //String stringYearsOfExperience = yearsOfExperience_field.getText().toString();
+        //yearsOfExperience = new Integer(stringYearsOfExperience).intValue();
+        //subjects = subjects_field.getText().toString();
 
 //        phone= phone_field.getText().toString();
 ////        email= email_field.getText().toString();
